c80693b1cbad278ed9cb1db845c02e47350fc726
PC-Ngumoha <pcngumoha@gmail.com>
2024-06-07 14:16:54 +0100

    Update: Refactor Image upload into a component. Additional UI and functional changes made on the Compose page.

---
+++ simpleblog/frontend/components/ImageUpload/ImageUpload.jsx
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faImage } from '@fortawesome/free-regular-svg-icons';
+import styles from './ImageUpload.module.css';
+
+export default function ImageUpload({
+  image, fileName, setImage, setFileName
+}) {
+
+  const handleChangeImage = (evt) => {
+    evt.preventDefault();
+    setImage(URL.createObjectURL(evt.target.files[0]));
+    setFileName(evt.target.files[0].name);
+  };
+
+  const handleDropImage = (evt) => {
+    evt.preventDefault();
+    const droppedFiles = evt.dataTransfer.files;
+
+    if (droppedFiles.length > 0) {
+      setImage(URL.createObjectURL(droppedFiles[0]));
+      setFileName(droppedFiles[0].name);
+    }
+  };
+
+  return (
+    <label htmlFor="blogImage"
+        className={ styles.imageArea }
+        onDrop={handleDropImage}
+        onDragOver={(evt) => evt.preventDefault()}>
+      {
+        image ? (
+          <>
+            <span>
+              <FontAwesomeIcon
+                icon={ faCircleXmark }
+                className={ styles.previewClose }
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  setImage(undefined);
+                  setFileName('');
+                }}
+              />
+            </span>
+            <img
+              src={ image }
+              alt={ fileName }
+            />
+            <span><b>File Name:</b> { fileName }</span>
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={ faImage }/>
+            <span>Click / Drag to upload thumbnail (.png, .jpg files only)</span>
+          </>
+        )
+      }
+
+      <input
+        type="file"
+        id="blogImage"
+        accept="image/jpeg image/jpg image/png"
+        onChange={handleChangeImage}
+      />
+    </label>
+  );
+}
+
+ImageUpload.propTypes = {
+  image: PropTypes.any,
+  fileName: PropTypes.string.isRequired,
+  setImage: PropTypes.func.isRequired,
+  setFileName: PropTypes.func.isRequired,
+};
--- simpleblog/frontend/pages/Compose/Compose.jsx
+++ simpleblog/frontend/pages/Compose/Compose.jsx
@@ -1,71 +1,20 @@
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faImage, faCircleXmark } from '@fortawesome/free-regular-svg-icons';
-// import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import ImageUpload from '../../components/ImageUpload/ImageUpload';
 import styles from './Compose.module.css';
 
 export default function Compose() {
   const [image, setImage] = useState();
   const [fileName, setFileName] = useState('');
 
-
-  const handleChangeImage = (evt) => {
-    evt.preventDefault();
-    setImage(URL.createObjectURL(evt.target.files[0]));
-    setFileName(evt.target.files[0].name);
-  };
-
-  const handleDropImage = (evt) => {
-    evt.preventDefault();
-    const droppedFiles = evt.dataTransfer.files;
-
-    if (droppedFiles.length > 0) {
-      setImage(URL.createObjectURL(droppedFiles[0]));
-      setFileName(droppedFiles[0].name);
-    }
-  };
-
   return (
     <form className={ styles.formArea }>
-      <label htmlFor="blogImage"
-        className={ styles.imageArea }
-        onDrop={handleDropImage}
-        onDragOver={(evt) => evt.preventDefault()}>
-        {
-          image ? (
-            <>
-              <span>
-                <FontAwesomeIcon
-                  icon={ faCircleXmark }
-                  className={ styles.previewClose }
-                  onClick={(evt) => {
-                    evt.preventDefault();
-                    setImage(undefined);
-                    setFileName('');
-                  }}
-                />
-              </span>
-              <img
-                src={ image }
-                alt={ fileName }
-              />
-              <span><b>File Name:</b> { fileName }</span>
-            </>
-          ) : (
-            <>
-              <FontAwesomeIcon icon={ faImage }/>
-              <span>Click / Drag to upload thumbnail (.png, .jpg files only)</span>
-            </>
-          )
-        }
+      <ImageUpload
+        image={image}
+        fileName={fileName}
+        setImage={setImage}
+        setFileName={setFileName}
+      />
 
-        <input
-          type="file"
-          id="blogImage"
-          accept="image/jpeg image/jpg image/png"
-          onChange={handleChangeImage}
-        />
-      </label>
       <div>
         <label htmlFor="title">Title: </label>
         <input type="text" id="title"/>
